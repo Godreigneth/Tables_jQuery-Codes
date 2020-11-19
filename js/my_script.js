78553cbfab006835e1740a4c6bb4b1dd3c578a2b
Godreigneth <godreignethlevidibia@yahoo.com>
2020-11-19 10:51:33 +0000

    jQuery addClass and removeClass functions applied on my_script.js file

---
+++ js/my_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   $("h2").addClass("underline"); //uderlines all <h2> elements 
+   $("ul").addClass("border"); //adds a border to all lists
+
+   
+   /** $("h2").addClass("underline"); //removes uderlines on all <h2> elements 
+   $("ul").addClass("border"); //removes a border to all lists
+   **/
+});
